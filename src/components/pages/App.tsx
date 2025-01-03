2b79cd25b4f1db69428cbc23a0d99471bc873859
Beverly Bembeka <beverly.bembeka@iMac-de-kadea.local>
2025-01-03 14:39:24 +0100

    correction de app

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,27 +1,38 @@
-import { Routes, Route, HashRouter } from 'react-router-dom'; // Remplace BrowserRouter par HashRouter
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import Services from './Services';
 import Contact from '../Contact';
+
 import Projects from '../pages/Projects';
+
+
 import Page from '../Page';
 import Shop from './Shops';
 import Home from './Home';
 import OrderForm from '../order/OrderForm';
 
+
+
 function App() {
   return (
-    <HashRouter> {/* Remplace BrowserRouter par HashRouter */}
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Page />}>
+       <Route path="/" element={<Page/>}>
           <Route index element={<Home />} />
           <Route path="services" element={<Services />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="projets" element={<Projects />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="formulaire" element={<OrderForm />} />
+         
+         
+          <Route path="contact" element={<Contact/>} />
+          <Route path="projets" element={<Projects/>} />
+         
+          <Route path="shop" element={<Shop/>} />
+          <Route path="formulaire" element={<OrderForm/>} />
         </Route>
-      </Routes>
-    </HashRouter>
+         
+        </Routes>
+    
+ 
+    </BrowserRouter>
   );
 }
 
